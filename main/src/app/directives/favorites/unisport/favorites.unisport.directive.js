6d144dc4f9706cf4c74eab989f22dbe744d451d3
Juho Rautio <juho.rautio@webscale.fi>
2016-08-30 15:18:09 +0300

    Enabled unisport favorite and unisport authorization url is decorated with redirect target url

--- main/src/app/directives/favorites/unisport/favorites.unisport.directive.js
+++ main/src/app/directives/favorites/unisport/favorites.unisport.directive.js
@@ -34,7 +34,7 @@ angular.module('directives.favorites.unisport', [
             event.timeRange = getEventTimeRange(event.startTime, event.endTime);
             return event;
           }).value();
-          $scope.authorizationUrl = data.authorizationUrl;
+          $scope.authorizationUrl = !_.isNull(data.authorizationUrl) ? data.authorizationUrl + '?redirectTarget=' + window.location.origin : null;
         });
 
         function getEventTimeRange(startTime, endTime) {
