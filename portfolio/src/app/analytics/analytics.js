282bdf35bf244077fdd876cf28de5adf6f65c663
Marko Rautajoki <marko.rautajoki@gofore.com>
2016-10-05 12:49:00 +0300

    Analytics for portfolio

---
+++ portfolio/src/app/analytics/analytics.js
@@ -0,0 +1,40 @@
+angular.module('portfolioAnalytics', [])
+
+  .constant('PORTFOLIO_PATH', '/portfolio')
+  .constant('TRACK_PAGE_TITLE', 'Portfolio')
+  .constant('ANALYTICS_STATE_DIMENSION', 'dimension3')
+
+  .config(function(AnalyticsProvider) {
+    if (window.configuration) {
+      AnalyticsProvider.setAccount(
+        {
+          tracker: window.configuration.googleAnalyticsAccount,
+          trackEvent: true
+        });
+
+      AnalyticsProvider.trackPages(false);
+    }
+  })
+
+  .factory('AnalyticsService', function(StateService,
+                                        Analytics,
+                                        PORTFOLIO_PATH,
+                                        TRACK_PAGE_TITLE,
+                                        ANALYTICS_STATE_DIMENSION,
+                                        $location) {
+
+    var urlWithoutUsername = $location.absUrl().replace(/\/[^\/]+$/, '');
+
+    Analytics.set(ANALYTICS_STATE_DIMENSION, StateService.getCurrent());
+
+    function trackPageView() {
+      Analytics.trackPage(PORTFOLIO_PATH, '', {
+        location: urlWithoutUsername,
+        referer: urlWithoutUsername,
+        title: TRACK_PAGE_TITLE});
+    }
+
+    return {
+      trackPageView: trackPageView
+    };
+  });
