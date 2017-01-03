2588acb9d75bcfa2fecfb61a86f0fe2eabc41ec4
David Price <david.price@codento.com>
2017-01-03 15:13:32 +0200

    Don’t need on-enter, we can use ui-keypress (used elsewhere for this purpose)

--- portfolio/src/app/directives/onEnterOrClick/onEnterOrClick.directive.js
+++ portfolio/src/app/directives/onEnterOrClick/onEnterOrClick.directive.js
@@ -35,19 +35,4 @@ angular.module('directives.onEnterOrClick', [])
         element.on('click', callBack);
       }
     };
-  })
-  .directive('onEnter', function() {
-    return {
-      restrict: 'A',
-      link: function(scope, element, attrs) {
-        element.on('keydown', function(event) {
-          if (event.which === 13) {
-            scope.$apply(function() {
-              scope.$eval(attrs.onEnter);
-            });
-            event.preventDefault();
-          }
-        });
-      }
-    };
   });
