5f7e52d7147b27fbc579f1d664ed910389fb4738
Henri Heiskanen <henri.heiskanen@gofore.com>
2017-01-10 14:59:31 +0200

    accommodate path change

--- main/karma.conf.js
+++ main/karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      '../bower_components/jquery/dist/jquery.js',
+      '../bower_components/jquery/jquery.js',
       '../bower_components/angular/angular.js',
       '../bower_components/angular-ui-router/release/angular-ui-router.js',
       '../bower_components/angular-resource/angular-resource.js',
