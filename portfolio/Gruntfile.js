61e0d04749455b31795081de6ef9e9a67d63d38d
Jukka Purma <jukka.purma@aalto.fi>
2018-07-03 10:33:09 +0300

    Make avatar image to ignore fingerprinting

--- portfolio/Gruntfile.js
+++ portfolio/Gruntfile.js
@@ -191,7 +191,8 @@ module.exports = function(grunt) {
           'dist/assets/styles/*.css',
           'dist/assets/fonts/hy-icons.*',
           'dist/assets/images/**/*.{png,ico,jpg}',
-          'dist/assets/icons/**/*.{png,svg}'
+          'dist/assets/icons/**/*.{png,svg}',
+          '!dist/assets/icons/avatar.png'
         ]
       }
     },
