d5e1c1fb5bd5ca9b9eb69535e16fea29364107ae
Marko Rautajoki <markorautajoki@gmail.com>
2018-09-12 15:19:33 +0300

    Fix lint

--- main/src/assets/bar/bar.js
+++ main/src/assets/bar/bar.js
@@ -1,3 +1,20 @@
+/*
+ * This file is part of MystudiesMyteaching application.
+ *
+ * MystudiesMyteaching application is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MystudiesMyteaching application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MystudiesMyteaching application.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 (function() {
 
   var htmlTemplate =
@@ -11,7 +28,11 @@
     '</div>' +
     '</div>' +
     '</div>' +
-    '<nav class="hy-bar__nav"><div class="l-top-bar__subregion"><ul><li><a href="/">etusivu</a></li><li><a href="/coursepages">opetus</a></li></ul></div></nav>' +
+    '<nav class="hy-bar__nav">' +
+    '<div class="l-top-bar__subregion">' +
+    '<ul><li><a href="/">etusivu</a></li><li><a href="/coursepages">opetus</a></li></ul><' +
+    '/div>' +
+    '</nav>' +
     '</div>';
 
   $('body').prepend(htmlTemplate);
