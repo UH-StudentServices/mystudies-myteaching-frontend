abe782b8a02936ee461773737b9f6359aadad521
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-09-21 10:38:38 +0300

    Add Academia.edu contact link

--- portfolio/src/app/directives/contactInformation/contactInformation.directive.js
+++ portfolio/src/app/directives/contactInformation/contactInformation.directive.js
@@ -22,15 +22,17 @@ angular.module('directives.contactInformation', ['services.contactInformation', 
     YOUTUBE: 'YOUTUBE',
     TWITTER: 'TWITTER',
     TUHAT: 'TUHAT',
-    RESEARCH_GATE: 'RESEARCH_GATE'
+    RESEARCH_GATE: 'RESEARCH_GATE',
+    ACADEMIA: 'ACADEMIA'
   })
 
   .factory('StudentSocialMediaLinks', function(SomeLinkType) {
-    return [SomeLinkType.TWITTER, SomeLinkType.FACEBOOK, SomeLinkType.YOUTUBE];
+    return [SomeLinkType.TWITTER, SomeLinkType.FACEBOOK, SomeLinkType.YOUTUBE,
+            SomeLinkType.TUHAT, SomeLinkType.RESEARCH_GATE, SomeLinkType.ACADEMIA];
   })
 
   .factory('TeacherSocialMediaLinks', function(SomeLinkType) {
-    return [SomeLinkType.TWITTER, SomeLinkType.TUHAT, SomeLinkType.RESEARCH_GATE];
+    return [SomeLinkType.TWITTER, SomeLinkType.TUHAT, SomeLinkType.RESEARCH_GATE, SomeLinkType.ACADEMIA];
   })
 
   .directive('contactInformation', function(ContactInformationService,
