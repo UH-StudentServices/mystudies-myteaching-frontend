bc24f280245a7001297f452339d5953d8015d97f
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-09-28 16:11:47 +0300

    Add styles.js to build

--- portfolio/Gruntfile.js
+++ portfolio/Gruntfile.js
@@ -273,7 +273,8 @@ module.exports = function(grunt) {
             'config.js',
             'editor.css',
             'lang/*',
-            'skins'
+            'skins',
+            'styles.js'
           ]
         }
         ]
