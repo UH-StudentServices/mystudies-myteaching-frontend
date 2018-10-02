6494cad0a8767e8afb6800cc75745053bb06df47
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-10-02 10:37:26 +0300

    Some images should be handled by filerev task

--- portfolio/Gruntfile.js
+++ portfolio/Gruntfile.js
@@ -190,7 +190,8 @@ module.exports = function(grunt) {
           'dist/app/newrelic.js',
           'dist/assets/styles/*.css',
           'dist/assets/fonts/hy-icons.*',
-          'dist/assets/images/**/*.{png,ico,jpg}',
+          'dist/assets/images/bg/*.{png,ico,jpg}',
+          'dist/assets/images/ui/*.{png,ico,jpg}',
           'dist/assets/icons/**/*.{png,svg}',
           '!dist/assets/icons/avatar.png'
         ]
