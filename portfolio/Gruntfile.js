4e2de6ade0d6ed89eccb33dc0c27015f06f682f9
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-10-09 12:40:52 +0300

    Copy editor plugins to build

--- portfolio/Gruntfile.js
+++ portfolio/Gruntfile.js
@@ -260,7 +260,10 @@ module.exports = function (grunt) {
             dot: true,
             cwd: '../bower_components/tinymce',
             dest: '<%= application.dist %>',
-            src: ['skins/**']
+            src: [
+              'skins/**',
+              'plugins/**/*'
+            ]
           }
         ]
       },
